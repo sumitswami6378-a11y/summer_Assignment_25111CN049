5feb942ba0380d5039482437281bf9b42dcf1a79
Sumit Swami <sumitswami6378@gmail.com>
2026-06-10 10:22:15 +0530

    q1 upload

---
+++ q1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n, sum = 0;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        sum += i; // Add the current number to sum
+    }
+
+    printf("The sum of the first %d natural numbers is: %d\n", n, sum);
+
+    return 0;
+}
